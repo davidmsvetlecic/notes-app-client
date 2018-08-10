2c8103269e14765dedd5556ce489a8e891127da8
David Svetlecic <DSvetlecic@cylance.com>
2018-08-10 17:43:45 -0500

    Add Login View and Route

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "app-scratch-notes-uploads"
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://u0d91vnsaf.execute-api.us-east-1.amazonaws.com/"
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_syqxGZIzG",
+    APP_CLIENT_ID: "4v6g23hh0ccf7b8fttckfjl6re",
+    IDENTITY_POOL_ID: "us-east-1:5fb7987d-d2b1-455d-888d-9acabff7617d"
+  }
+};
